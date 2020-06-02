eea540e1da8173771ac2fb0550575c293b1f2817
Meghdad <meghdadd78@gmail.com>
2020-06-03 00:26:18 +0330

    rna transcription changed

--- rna-transcription/rna-transcription.ts
+++ rna-transcription/rna-transcription.ts
@@ -1,6 +1,17 @@
 class Transcriptor {
-    toRna( /* Parameters go here */ ) {
-        // Your code here
+    toRna(DNAVals: string[]) {
+      for (let index = 0; index < DNAVals.length; index++) {
+        const element = DNAVals[index];
+        if (element == "G")
+          DNAVals[index] = "C";
+        else if (element == "C")
+          DNAVals[index] = "G";
+        else if (element == "T")
+          DNAVals[index] = "A";
+        else if (element == "A")
+          DNAVals[index] = "U";
+      }
+      return DNAVals;
     }
 }
 
