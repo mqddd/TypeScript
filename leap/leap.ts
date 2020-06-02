f27bccd55f27f8f65d58ae499499c9dc077d9b85
Meghdad <meghdadd78@gmail.com>
2020-06-03 00:21:27 +0330

    leap changed

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,5 +1,12 @@
-function isLeapYear( /* Parameters go here */ ) {
-    // Your code here
+function isLeapYear(year: number) {
+  if (year/400 == Math.floor(year/400))
+    return "true";
+  else if (year/100 == Math.floor(year/100))
+    return "false";
+  else if (year/4 == Math.floor(year/4))
+    return "true";
+  else
+    return "false";
 }
 
 export default isLeapYear
