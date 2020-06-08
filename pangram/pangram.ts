7b57564993aad93ef8a35968125683f8fe8a9c08
Meghdad <meghdadd78@gmail.com>
2020-06-08 20:23:54 +0330

    pangram changed

--- pangram/pangram.ts
+++ pangram/pangram.ts
@@ -5,24 +5,26 @@ class Pangram {
       this.sentence = sentence;
   }
 
-  isPangram = (): boolean => {
+  public isPangram(): boolean {
+    return checkIfPangram();
+  }
+
+  private checkIfPangram(): boolean {
     let size: number = this.sentence.length;
     let lowerSentence: string = this.sentence.toLowerCase();
     let letters: string[] = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     let count: number = 0;
-    for (let index = 0; index < size; index++) {
-      const char = lowerSentence[index];
-      for (let index = 0; index < 26; index++) {
-        const element = letters[index];
-        if (element == char) {
+    this.sentence.forEach((char: string) => {
+      letters.forEach((lettersChar: string, index: number) => {
+        if (lettersChar === char){
           count++;
           letters.splice(index, 1);
         }
-      }
-    }
-    if (count == 26) {
+      });
+    });
+    if (count === 26)
       return true;
-    }
-    return false;
+    else
+      return false;
   }
 }
