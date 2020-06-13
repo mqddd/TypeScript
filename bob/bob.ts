0ebd32f6bc704ddab9b6364e41c11480fc95061b
Meghdad <meghdadd78@gmail.com>
2020-06-14 02:24:57 +0330

    bob changed

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,7 +1,26 @@
 class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+
+    hey(sentence: string): string {
+        return checkEndingChar(sentence);
     }
+
+    private checkEndingChar(sentence: string): string {
+      while (sentence.endsWith(' ')) {
+        sentence.pop();
+      }
+      if (sentence.length === 0) {
+        return 'Fine, Be that way!';
+      } else {
+        if (sentence.endsWith('.')) {
+          return 'Whatever.';
+        } else if (sentence.endsWith('?')) {
+          return 'Sure.';
+        } else if (sentence.endsWith('!')) {
+          return 'Whoa, chill out!';
+        } else return 'Whatever.';
+      }
+    }
+
 }
 
 export default Bob
