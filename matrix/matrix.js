e6d642c444c8d4e54fa57aa18acfa6a0a11c6e31
Meghdad <meghdadd78@gmail.com>
2020-06-17 02:11:53 +0330

    matrix improved

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,17 +1,66 @@
 "use strict";
+/* let a = "1 3 5\n2 4 6";
+let b: string[] = a.split('\n');
+let c: number[][] = [[]];
+b.forEach((element, index) => {
+    c[index] = element.split(' ').map(tooo);
+});
+let d: number[][] = [[]];
+for (let i = 0; i < 3; i++) {
+    d[i] = new Array(2);
+    for (let index = 0; index < 2; index++) {
+        d[i][index] = c[index][i];
+    }
+}
+c.forEach((row: number[], index: number) => {
+    row.forEach((element: number, i: number) => {
+        d[i][index] = element;
+    });
+});
+function tooo(num: string): number{
+    return +num;
+}
+console.log(c[0].length); */
 exports.__esModule = true;
-var a = "1 2 3\n2 5 6";
-var b = a.split('\n');
-console.log(b[1]);
 var Matrix = /** @class */ (function () {
     function Matrix(matrix) {
-        this.matrix = matrix.split('');
+        this.matrix = matrix.split('\n');
+        this._rows = [[]];
+        this._columns = [[]];
+        this.makeRows();
+        this.makeColumns();
     }
     Matrix.prototype.makeRows = function () {
-        this.rows = this.matrix;
+        var _this = this;
+        this.matrix.forEach(function (row, index) {
+            _this._rows[index] = row.split(' ').map(_this.parseStringToNumber);
+        });
     };
     Matrix.prototype.makeColumns = function () {
+        for (var index = 0; index < this._rows[0].length / 2; index++) {
+            this._columns[index] = new Array(this._rows.length);
+            for (var i = 0; i < this._rows.length; i++) {
+                this._columns[index][i] = this._rows[i][index];
+            }
+        }
     };
+    Matrix.prototype.parseStringToNumber = function (str) {
+        return +str;
+    };
+    Object.defineProperty(Matrix.prototype, "rows", {
+        get: function () {
+            return this._rows;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Matrix.prototype, "columns", {
+        get: function () {
+            return this._columns;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Matrix;
 }());
 exports["default"] = Matrix;
--- matrix/matrix.ts
+++ matrix/matrix.ts
@@ -1,21 +1,68 @@
-let a = "1 2 3\n2 5 6";
-let b = a.split('\n');
-console.log(b[1]);
+/* let a = "1 3 5\n2 4 6";
+let b: string[] = a.split('\n');
+let c: number[][] = [[]];
+b.forEach((element, index) => {
+    c[index] = element.split(' ').map(tooo);
+}); 
+let d: number[][] = [[]];
+for (let i = 0; i < 3; i++) {
+    d[i] = new Array(2);
+    for (let index = 0; index < 2; index++) {
+        d[i][index] = c[index][i];
+    }
+}
+c.forEach((row: number[], index: number) => {
+    row.forEach((element: number, i: number) => {
+        d[i][index] = element;
+    });
+});
+function tooo(num: string): number{
+    return +num;
+}
+console.log(c[0].length); */
 
 
 
 class Matrix {
-        private rows: string[];
-        private columns: string[];
+        private _rows: number[][];
+        private _columns: number[][];
         private matrix: string[];
         constructor(matrix: string) {
-            this.matrix = matrix.split('');
+            this.matrix = matrix.split('\n');
+            this._rows = this.makeRows();
+            this._columns = this.makeColumns();
         }
-        private makeRows() {
-            this.rows = this.matrix;
+
+        private makeRows(): number[][] {
+            let tempRows: number[][] = [[]];
+            this.matrix.forEach((row: string, index: number) => {
+                tempRows[index] = row.split(' ').map(this.parseStringToNumber);
+            });
+            return tempRows;
         }
-        private makeColumns() {
-            
+
+        private makeColumns(): number[][] {
+            let tempColumns: number[][] = [];
+            let tempRows: number[][] = this.makeRows();
+            for (let index = 0; index < tempRows[0].length; index++) {
+                tempColumns[index] = new Array(tempRows.length);
+                for (let i = 0; i < tempRows.length; i++) {
+                    tempColumns[index][i] = this._rows[i][index];
+                }
+            }
+            return tempColumns;
+        }
+
+        private parseStringToNumber(str: string):number {
+            return +str;            
+        }
+
+        public get rows(): number[][] {
+            return this._rows;
+        }
+
+        public get columns(): number[][] {
+            return this._columns;
         }
 }
 
