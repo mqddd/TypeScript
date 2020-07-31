1caaec85b59a736c92e034f5cc1e50e4387c33f9
Meghdad <meghdadd78@gmail.com>
2020-07-31 20:46:56 +0330

    code finished

--- rational-numbers/rational-numbers.ts
+++ rational-numbers/rational-numbers.ts
@@ -17,14 +17,20 @@ class Rational{
     private simpleRational(r: Rational): Rational{
         const a: number = r.a;
         const b: number = r.b;
-        const min: number = Math.min(a, b);
+        const min: number = Math.abs(Math.min(a, b));
         let gcb: number = 1;
         for (let i = 2; i <= min; i++) {
             if (a % i === 0 && b % i === 0) {
                 gcb = i;
             }
         }
-        return new Rational(a / gcb, b / gcb);
+        if (a === 0) {
+            return new Rational(0, 1);
+        } else if ((a < 0 && b < 0) || (a > 0 && b < 0)) {
+            return new Rational((-1) * (a / gcb), (-1) * (b / gcb));
+        } else {
+            return new Rational(a / gcb, b / gcb);
+        }
     }
 
     public add(r: Rational): Rational{
@@ -32,7 +38,47 @@ class Rational{
         const top: number = (this.a * r.b) + (this.b * r.a);
         return this.simpleRational(new Rational(top, bottom));
     }
+
+    public sub(r: Rational): Rational{
+        const bottom: number = this.b * r.b;
+        const top: number = (this.a * r.b) - (this.b * r.a);
+        return this.simpleRational(new Rational(top, bottom));
+    }
+
+    public mul(r: Rational): Rational{
+        const top: number = this.a * r.a;
+        const bottom: number = this.b * r.b;
+        return this.simpleRational(new Rational(top, bottom));
+    }
+
+    public div(r: Rational): Rational{
+        const top: number = this.a * r.b;
+        const bottom: number = this.b * r.a;
+        return this.simpleRational(new Rational(top, bottom));
+    }
+
+    public abs(): Rational{
+        if (this.a <= 0) {
+            return this.simpleRational(new Rational((-1) * this.a, this.b));
+        } else {
+            return this;
+        }
+    }
+
+    public exprational(num: number): Rational{
+        const top: number = Math.pow(this.a, num);
+        const bottom: number = Math.pow(this.b, num);
+        return this.simpleRational(new Rational(top, bottom));
+    }
+
+    public expreal(num: number): number{
+        const p: number = this.a / this.b;
+        return Math.pow(num, p);
+    }
     
+    public reduce(): Rational{
+        return this.simpleRational(new Rational(this.a, this.b));
+    }
 }
 
 export default Rational;
